c224407e44efca5d409c1487ed9107b2e6ac1711
bigsem89 <bigsem89@gmail.com>
2023-01-23 20:59:54 +0200

    kube-prometheus-stack

--- gke.tf
+++ gke.tf
@@ -18,7 +18,7 @@ resource "google_container_cluster" "cluster" {
   ]
   addons_config {
     http_load_balancing {
-      disabled = true
+      disabled = false
     }
     horizontal_pod_autoscaling {
       disabled = false
--- packages.tf
+++ packages.tf
@@ -1,12 +1,14 @@
-resource "helm_release" "prometheus" {
-  name       = "v1"
+resource "helm_release" "kube-prometheus-stack" {
+  name       = "kube-prometheus-stack"
   create_namespace = true
   dependency_update = true
   cleanup_on_fail = true
   namespace = "monitoring"
   repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus"
-  version = "19.3"
+  chart      = "kube-prometheus-stack"
+  version = "44"#"19.3"
+  wait = true
+  wait_for_jobs = true
 
 #  set {
 #    name  = "namespace"
@@ -16,3 +18,24 @@ resource "helm_release" "prometheus" {
     google_container_cluster.cluster
   ]
 }
+
+#resource "helm_release" "grafana" {
+#  name       = "grafana"
+#  create_namespace = true
+#  dependency_update = true
+#  cleanup_on_fail = true
+#  namespace = "monitoring"
+#  repository = "https://grafana.github.io/helm-charts"
+#  chart      = "grafana"
+#  values = [
+#    file("grafana-values/values.yaml")
+#  ]
+#
+##  set {
+##    name  = "namespace"
+##    value = "prometheus"
+##  }
+#  depends_on = [
+#    google_container_cluster.cluster
+#  ]
+#}
